e31ba9a39ed8d0cfe3351b312a422e1344addb6f
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2018-11-08 19:23:04 +0100

    Menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Carrito de la compra");
+        System.out.println("------Menu----------");
+        System.out.println("Afegir");
     }
 }
